2b36521bb19a349f95d3f687bde3ac7874d87342
Skip0s <61255262+SkipOs@users.noreply.github.com>
2021-07-12 18:33:19 -0300

    Add files via upload

--- script/form_validation.js
+++ script/form_validation.js
@@ -1,8 +1,14 @@
 function validar() {
     var nome = formulario.nome.value;
+    var mail = formulario.mail.value;
     if (nome == "") {
         window.alert("Preencha o campo com seu nome!");
         formulario.nome.focus();
+        return false;
+    }
+    if (mail == "") {
+        window.alert("Preencha o campo com seu e-mail!");
+        formulario.mail.focus();
         return false;
     }
 }
